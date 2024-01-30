ca373dadf0d78c908459eba87772ee158f06f97f
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2024-01-30 11:51:19 +0530

    add : ethers_examples setup

--- examples/5_contract_event_stream.js
+++ examples/5_contract_event_stream.js
@@ -1,25 +1,25 @@
-const { ethers } = require("ethers");
+// const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+// const INFURA_ID = ''
+// const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
-const ERC20_ABI = [
-    "function name() view returns (string)",
-    "function symbol() view returns (string)",
-    "function totalSupply() view returns (uint256)",
-    "function balanceOf(address) view returns (uint)",
+// const ERC20_ABI = [
+//     "function name() view returns (string)",
+//     "function symbol() view returns (string)",
+//     "function totalSupply() view returns (uint256)",
+//     "function balanceOf(address) view returns (uint)",
 
-    "event Transfer(address indexed from, address indexed to, uint amount)"
-];
+//     "event Transfer(address indexed from, address indexed to, uint amount)"
+// ];
 
-const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
-const contract = new ethers.Contract(address, ERC20_ABI, provider)
+// const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
+// const contract = new ethers.Contract(address, ERC20_ABI, provider)
 
-const main = async () => {
-    const block = await provider.getBlockNumber()
+// const main = async () => {
+//     const block = await provider.getBlockNumber()
 
-    const transferEvents = await contract.queryFilter('Transfer', block - 1, block)
-    console.log(transferEvents)
-}
+//     const transferEvents = await contract.queryFilter('Transfer', block - 1, block)
+//     console.log(transferEvents)
+// }
 
-main()
+// main()
--- examples/6_inspecting_blocks.js
+++ examples/6_inspecting_blocks.js
@@ -1,21 +1,21 @@
-const { ethers } = require("ethers");
+// const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+// const INFURA_ID = ''
+// const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
-const main = async () => {
-    const block = await provider.getBlockNumber()
+// const main = async () => {
+//     const block = await provider.getBlockNumber()
 
-    console.log(`\nBlock Number: ${block}\n`)
+//     console.log(`\nBlock Number: ${block}\n`)
 
-    const blockInfo = await provider.getBlock(block)
+//     const blockInfo = await provider.getBlock(block)
 
-    console.log(blockInfo)
+//     console.log(blockInfo)
 
-    const { transactions } = await provider.getBlockWithTransactions(block)
+//     const { transactions } = await provider.getBlockWithTransactions(block)
 
-    console.log(`\nLogging first transaction in block:\n`)
-    console.log(transactions[0])
-}
+//     console.log(`\nLogging first transaction in block:\n`)
+//     console.log(transactions[0])
+// }
 
-main()
+// main()
